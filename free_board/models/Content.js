3a882ad84ee8043b14ba1c08a6e8c88d7a6dbb0e
jeonghwanPark <jpark0902@kookmin.ac.kr>
2021-02-10 23:06:47 +0900

    로그인 기능, 수정&삭제 권한 제어

--- free_board/models/Content.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require('mongoose');
-var contentSchema = mongoose.Schema({
-  name:{type:String, required:true},
-  content:{type:String, required:true},
-});
-var Content = mongoose.model('content', contentSchema);
-module.exports = Content;
